ad25878fbf96d3d4cc9bd602484304f004e490d4
popaprozac <zbotterman@gmail.com>
2025-02-22 01:29:28 -0800

    simplify

--- v3/pkg/services/badge/badge_darwin.m
+++ v3/pkg/services/badge/badge_darwin.m
@@ -2,14 +2,10 @@
 #import <Cocoa/Cocoa.h>
 
 void setBadgeLabel(const char *label) {
-    @autoreleasepool {
-        NSString *nsLabel = nil;
-        if (label != NULL) {
-            nsLabel = [NSString stringWithUTF8String:label];
-        }
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [[NSApp dockTile] setBadgeLabel:nsLabel];
-            [[NSApp dockTile] display];
-        });
-    }
+    NSString *nsLabel = nil;
+	if (label != NULL) {
+		nsLabel = [NSString stringWithUTF8String:label];
+	}
+	[[NSApp dockTile] setBadgeLabel:nsLabel];
+	[[NSApp dockTile] display];
 }
